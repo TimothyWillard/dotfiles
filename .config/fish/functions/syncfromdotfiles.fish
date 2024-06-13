8d6808463772903bb293ead4de645182fc45982d
Timothy Willard <timothywillard22@gmail.com>
2024-06-13 13:21:14 -0400

    Added my vscode settings to my dotfiles as well as edited syncfromdotfiles to sync that seperately

--- .config/fish/functions/syncfromdotfiles.fish
+++ .config/fish/functions/syncfromdotfiles.fish
@@ -1,4 +1,5 @@
 function syncfromdotfiles --description "Sync files from GitHub dotfiles"
-	rsync --recursive --checksum --verbose --times --exclude=.git ~/Desktop/GitHub/TimothyWillard/dotfiles/ ~/
+	rsync --recursive --checksum --verbose --times --exclude=.git --exclude=vscode_settings.json ~/Desktop/GitHub/TimothyWillard/dotfiles/ ~/
+	rsync --recursive --checksum --verbose --times --exclude=.git ~/Desktop/GitHub/TimothyWillard/dotfiles/vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
 	source ~/.config/fish/**/*.fish
 end
