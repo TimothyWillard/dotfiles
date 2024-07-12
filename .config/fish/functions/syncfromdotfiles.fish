cf72ac25045b5a2552a97812647f92e1742dd52e
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2024-07-11 22:26:03 -0400

    Adding config on home laptop for verified commits

--- .config/fish/functions/syncfromdotfiles.fish
+++ .config/fish/functions/syncfromdotfiles.fish
@@ -2,6 +2,8 @@ function syncfromdotfiles --description "Sync files from GitHub dotfiles"
 	rsync --recursive --checksum --verbose --times --exclude=.git --exclude=vscode_settings.json --exclude=".gitconfig-*" ~/Desktop/GitHub/TimothyWillard/dotfiles/ ~/
 	if string match --quiet -- 'epid-iss-mbp*' (hostname)
 		set gitconfiglocal '.gitconfig-work'
+	else if string match --quiet -- 'Timothys-MBP-3*' (hostname)
+		set gitconfiglocal '.gitconfig-home'
 	end
 	rsync --recursive --checksum --verbose --times --exclude=.git ~/Desktop/GitHub/TimothyWillard/dotfiles/$gitconfiglocal ~/.gitconfig-local
 	rsync --recursive --checksum --verbose --times --exclude=.git ~/Desktop/GitHub/TimothyWillard/dotfiles/vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
