e781e71cdb03da7b858762202d6c093287fc27ad
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2024-10-10 12:56:39 -0400

    Source conda.fish script to replicate conda init

--- .config/fish/functions/cup.fish
+++ .config/fish/functions/cup.fish
@@ -8,8 +8,13 @@ function cup --description "Conda up, either add conda to the path or remove it 
         # Is in path, need to remove
         set i (contains --index $HOME/anaconda3/bin $PATH)
         set -e PATH[$i]
+        if contains $HOME/anaconda3/condabin $PATH
+            set i (contains --index $HOME/anaconda3/condabin $PATH)
+            set -e PATH[$i]
+        end
     else
         # Is not in path, need to add
+        source $HOME/anaconda3/etc/fish/conf.d/conda.fish
         fish_add_path --path --prepend $HOME/anaconda3/bin
     end
 end
