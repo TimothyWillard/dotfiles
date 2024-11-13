09d8625d7c05084286db88a26f808088ae3232f9
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2024-11-13 09:28:13 -0500

    Re-org and comments for `config.fish`.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,32 +3,47 @@ if status is-interactive
 end
 
 # Need to manually set SDK root for R CMD Check
-if string match --quiet -- 'epid-iss-mbp*' (hostname)
+if string match --quiet -- 'epid-iss-*' (hostname)
     set -Ux SDKROOT (xcrun --show-sdk-path)
 end
 
+# Fish shell color 
 set -Ux fish_color_command dade73
-# https://stackoverflow.com/a/25563976
-set -Ux LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
+
+# gpg tty for git gpg signing
 set -x GPG_TTY (tty)
 
-source $HOME/.cargo/env.fish
-fish_add_path ~/.local/bin
+# Fix directory colors
+if test (uname) = "Darwin"
+    # https://stackoverflow.com/a/25563976
+    set -Ux LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
+end
 
+# Cargo fish highlighting/autocomplete
+if test -f "$HOME/.cargo/env.fish"
+    source $HOME/.cargo/env.fish
+end
+
+# User ~/.local/bin
+if test -d "$HOME/.local/bin"
+    fish_add_path ~/.local/bin
+end
+
+# Bun JS
+if test -d "$HOME/.bun"
+    set --export BUN_INSTALL "$HOME/.bun"
+    set --export PATH $BUN_INSTALL/bin $PATH
+end
+
+# Aliases
 alias lah="ls -lah"
 alias lh="ls -lah"
 alias rfish="source ~/.config/fish/**/*.fish; source ~/.config/fish/config.fish"
-
-alias mmdb="movemydockback"
 alias sfdf="syncfromdotfiles"
-
 alias gti="git"
 alias igt="git"
-
-alias pyink="$HOME/venvs/pyink_venv/bin/python -m pyink"
-
-alias gchrome='open -a "Google Chrome"'
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
+if test (uname) = "Darwin"
+    # MacOS specific aliases
+    alias gchrome='open -a "Google Chrome"'
+    alias mmdb="movemydockback"
+end
