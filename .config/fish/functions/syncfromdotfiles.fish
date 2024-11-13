d776ce763ad215f16cdd0c74f22dd6e607d6b481
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2024-11-13 10:50:16 -0500

    Update `syncfromdotfiles` to be more dynamic
    Now handles different dotfiles locations and only syncs the vscode
    settings if the settings location exists.

--- .config/fish/functions/syncfromdotfiles.fish
+++ .config/fish/functions/syncfromdotfiles.fish
@@ -1,11 +1,23 @@
 function syncfromdotfiles --description "Sync files from GitHub dotfiles"
-	rsyncsansgit --exclude=vscode_settings.json --exclude=".gitconfig-*" ~/Desktop/GitHub/TimothyWillard/dotfiles/ ~/
+	# Determine where to source dotfiles from
+	if test -d "$HOME/Desktop/GitHub/TimothyWillard/dotfiles"
+		set DOTFILES $HOME/Desktop/GitHub/TimothyWillard/dotfiles
+	else if test -d "$HOME/dotfiles"
+		set DOTFILES $HOME/dotfiles
+	else
+		echo "Unable to determine where to source dotfiles from."
+		return 1
+	end
+	# Place
+	rsyncsansgit --exclude=vscode_settings.json --exclude=".gitconfig-*" $DOTFILES/ $HOME/
 	if string match --quiet -- 'epid-iss-*' (hostname)
 		set gitconfiglocal '.gitconfig-work'
 	else if string match --quiet -- 'Timothys-*' (hostname)
 		set gitconfiglocal '.gitconfig-home'
 	end
-	rsyncsansgit ~/Desktop/GitHub/TimothyWillard/dotfiles/$gitconfiglocal ~/.gitconfig-local
-	rsyncsansgit ~/Desktop/GitHub/TimothyWillard/dotfiles/vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
+	rsyncsansgit $DOTFILES/$gitconfiglocal $HOME/.gitconfig-local
+	if test -d "$HOME/Library/Application Support/Code/User"
+		rsyncsansgit $DOTFILES/vscode_settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+	end
 	source ~/.config/fish/**/*.fish
 end
