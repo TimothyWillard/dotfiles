8a785553c25ede2ad4cd7d1392660e8125c6bdbc
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-07-11 09:28:53 -0400

    `ghclone` defaults to `jj`, exclude '.jj' from rsync
    * Made `jj` the default vcs for `ghclone` and added the --git flag to
    	fall back to git, and
    * Exclude '.jj' directories when using `rsyncsansgit` alias which is
    	used by `syncfromdotfiles`.
    
    See #8.

--- .config/fish/functions/ghclone.fish
+++ .config/fish/functions/ghclone.fish
@@ -4,12 +4,14 @@ function ghclone --description "Shorthand for cloning a repo from GitHub to the 
 		return 1
 	end
 	mkdir -vp $HOME/Desktop/GitHub/$repo
-	if contains -- --worktree $argv; or contains -- -w $argv
+	if contains -- --git $argv; or contains -- -g $argv
+		git clone --verbose git@github.com:$repo.git ~/Desktop/GitHub/$repo
+	else if contains -- --worktree $argv; or contains -- -w $argv
 		# https://infrequently.org/2021/07/worktrees-step-by-step/
 		git clone --verbose --bare git@github.com:$repo.git ~/Desktop/GitHub/$repo/.bare
 		echo "gitdir: ./.bare" > ~/Desktop/GitHub/$repo/.git
 		git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 	else
-		git clone --verbose git@github.com:$repo.git ~/Desktop/GitHub/$repo
+		jj git clone --verbose git@github.com:$repo.git ~/Desktop/GitHub/$repo
 	end
 end
