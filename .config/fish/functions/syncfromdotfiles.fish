aa18839606b10863ad47d222850fc6ffc9ee44dd
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-07-16 13:42:43 -0400

    `jj` config syncing, #8

--- .config/fish/functions/syncfromdotfiles.fish
+++ .config/fish/functions/syncfromdotfiles.fish
@@ -8,13 +8,35 @@ function syncfromdotfiles --description "Sync files from GitHub dotfiles"
 		echo "Unable to determine where to source dotfiles from."
 		return 1
 	end
-	# Place
-	rsyncsansgit --exclude=vscode_settings.json --exclude=".gitconfig-*" $DOTFILES/ $HOME/
+	set osname (uname)
+	# Place config files in the right place
+	echo "> Syncing fish config"
+	rsyncsansgit $DOTFILES/.config/fish/ $HOME/.config/fish/
+	if which jj > /dev/null 2>&1
+		echo "> Syncing jj config"
+		set jjconfigpath (jj config path --user)
+		rsyncsansgit $DOTFILES/jj_config.toml $jjconfigpath
+		if [ "$osname" = "Darwin" ]
+			set cptrname (scutil --get ComputerName)
+			echo "Computer name: $cptrname"
+			if string match --quiet -- 'epid_id*MacBook Pro' $cptrname
+				gsed -i 's/key = ""/key = "871B573E01D0FB85"/' $jjconfigpath
+			else if string match --quiet -- 'Timothy*' $cptrname
+				gsed -i 's/key = ""/key = "D9EEB52CF5D0CC09"/' $jjconfigpath
+			end
+		else if [ "$osname" = "Linux" ]
+			set cptrname (hostnamectl --static)
+			if string match --quiet -- 'longleaf-*.unc.edu' $cptrname
+				gsed -i 's/key = ""/key = "EB5E2D91340E42B5"/' $jjconfigpath
+			end
+		end
+	end
+	echo "> Syncing git config"
 	if [ "$osname" = "Darwin" ]
         set cptrname (scutil --get ComputerName)
 		if string match --quiet -- 'epid_id*MacBook Pro' $cptrname
 			set gitconfiglocal '.gitconfig-work'
-		else if string match --quiet -- 'Timothys-*' $cptrname
+		else if string match --quiet -- 'Timothy*' $cptrname
 			set gitconfiglocal '.gitconfig-home'
 		end
     else if [ "$osname" = "Linux" ]
@@ -28,8 +50,14 @@ function syncfromdotfiles --description "Sync files from GitHub dotfiles"
 	else
 		echo "Unable to find a local gitconfig to sync"
 	end
+	rsyncsansgit $DOTFILES/.gitconfig $HOME/.gitconfig
 	if test -d "$HOME/Library/Application Support/Code/User"
+		echo "> Syncing VSCode settings"
 		rsyncsansgit $DOTFILES/vscode_settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 	end
+	echo "> Syncing other dotfiles"
+	rsyncsansgit $DOTFILES/.bashrc $HOME/.bashrc
+	rsyncsansgit $DOTFILES/.vimrc $HOME/.vimrc
+	rsyncsansgit $DOTFILES/.zprofile $HOME/.zprofile
 	source ~/.config/fish/**/*.fish
 end
